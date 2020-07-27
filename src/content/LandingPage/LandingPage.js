3a1776a8753aa8446e47e28ca9db4cb5d90b7c1e
Mehdi Merali <mmerali@ibm.com>
2020-07-26 21:12:38 -0400

    created initial ProfilePage and LandingPage

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class LandingPage extends React.Component {
+  render() {
+    return <h1>Landing page</h1>;
+  }
+}
+
+export default LandingPage;
---
+++ src/content/ProjectPage/ProjectPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ProjectPage extends React.Component {
+  render() {
+    return <h1>Project page</h1>;
+  }
+}
+
+export default ProjectPage;
